c2c6813364d19dffc6fbe6e01bbf2c466c4d39b2
mzfira <mzfira@gmail.com>
2024-07-05 01:08:36 +0300

    refined ac. , created small db from json files

--- site/myScript.js
+++ site/myScript.js
@@ -3,9 +3,65 @@ const sub = document.getElementById("guess-submit");
 const guessBox = document.getElementById("guess-box");
 const wrongGuesses = document.getElementById("wrong-guesses");
 const guessWrapper = document.getElementById("guess-box-wrapper");
-// const pDB = new Map([
-//     [""
-// ])
+const pDB = new Map([
+    ["Ofir Marciano", ["Israel", "MSA", "GK", "33", "1"]],
+    ["Miguel Vítor", ["Portugal", "HBS", "DF", "33", "4"]],
+    ["Sheran Yeini", ["Israel", "MTA", "DF", "36", "21"]],
+    ["Lior Refaelov", ["Israel", "MHDR", "MF", "37", "10"]],
+    ["Dan Einbinder", ["Israel", "HTA", "MF", "34", "8"]],
+    ["Eyad Hutba", ["Israel", "MBR", "DF", "29", "18"]],
+    ["Ram Levy", ["Israel", "HPT", "MF", "27", "23"]],
+    ["Yonas Malede", ["Israel", "BJFC", "FW", "23", "9"]],
+    ["Mohammed Hindy", ["Israel", "MPT", "DF", "24", "4"]],
+    ["Ofek Bitton", ["Israel", "HJE", "MF", "22", "6"]],
+    ["Tomer Josefi", ["Israel", "HPFC", "MF", "25", "17"]],
+    ["Aviv Avraham", ["Israel", "MNT", "MF", "27", "19"]],
+    ["Maroun Gantous", ["Israel", "BNS", "DF", "25", "5"]],
+    ["Barak Bachar", ["Israel", "MHDR", "Man Ar", "43", ""]],
+    ["Eli Dasa", ["Israel", "MHDR", "DF", "30", "2"]],
+    ["Omer Atzili", ["Israel", "MHDR", "FW", "29", "7"]],
+    ["Neta Lavi", ["Israel", "MHDR", "MF", "27", "6"]],
+    ["Eylon Almog", ["Israel", "MTA", "FW", "23", "17"]],
+    ["Dan Glazer", ["Israel", "MTA", "MF", "26", "6"]],
+    ["Stipe Perica", ["Croatia", "MTA", "FW", "28", "99"]],
+    ["Eran Zahavi", ["Israel", "MTA", "FW", "35", "7"]],
+    ["Ben Bitton", ["Israel", "HTA", "DF", "31", "2"]],
+])
+window.addEventListener("keydown", (eve) => {
+    // eve.preventDefault();
+    const fcs = document.activeElement;
+    let key = eve.key;
+    if ((key === "ArrowDown" || key === "Tab") && fcs.getAttribute("id") == "guess-box") {
+        eve.preventDefault();
+        const newFcs = fcs.parentElement.lastElementChild.firstElementChild.nextElementSibling;
+        newFcs.focus();
+
+        guessBox.value = newFcs.innerHTML;
+    }
+    if ((key === "ArrowDown" || key === "Tab") && fcs.getAttribute("class") == "match no-hover") {
+        eve.preventDefault();
+        if (fcs.nextElementSibling != null) {
+            const newFcs = fcs.nextElementSibling;
+            newFcs.focus();
+            guessBox.value = newFcs.innerHTML;
+        }
+    }
+    if (key === "ArrowUp" && fcs.getAttribute("class") == "match no-hover") {
+        eve.preventDefault();
+        if (fcs.previousElementSibling.getAttribute("class") == "no-match") {
+            guessBox.focus();
+            guessBox.value = "";
+        }
+        else {
+            const newFcs = fcs.previousElementSibling;
+            newFcs.focus();
+            guessBox.value = newFcs.innerHTML;
+        }
+    }
+    if (key === "Enter" && fcs.getAttribute("class") == "match no-hover") {
+        guessBox.focus();
+    }
+})
 guessBox.addEventListener("input", (ev) => {
 
     let val = guessBox.value;
@@ -25,10 +81,11 @@ guessBox.addEventListener("input", (ev) => {
 
 function autoComp(val) {
     const ac = Object.assign(document.createElement("div"), { className: "container", id: "ac-box" });
+    ac.addEventListener("mousemove", focusedThenHover, false);
     const nomatch = Object.assign(document.createElement("div"), { className: "no-match" });
     ac.appendChild(nomatch);
-    // const matches = posMatches(val, pDB);
-    const matches = ["ערן זהבי", "אבי נמני", "יניב קטן", "דקל קינן", "אייל גולסה"]
+    const matches = posMatches(val, pDB);
+    // const matches = ["ערן זהבי", "אבי נמני", "יניב קטן", "דקל קינן", "אייל גולסה"]
     for (x in matches) {
         const match = Object.assign(document.createElement("div"), { className: "match", innerHTML: matches[x] });
         match.setAttribute("tabindex", "0");
@@ -37,31 +94,61 @@ function autoComp(val) {
     guessWrapper.appendChild(ac);
     var matchesSelectors = document.getElementsByClassName("match");
     for (i = 0; i < matchesSelectors.length; i++) {
-        matchesSelectors[i].addEventListener("click", myFunc, false);
+        matchesSelectors[i].addEventListener("click", selectFromAC, false);
+        matchesSelectors[i].addEventListener("focus", hoverDis, false);
+        matchesSelectors[i].addEventListener("blur", hoverAct, false);
+        matchesSelectors[i].addEventListener("mouseover", hoverAct, false);
+
     }
 }
-function myFunc() {
-    guessBox.value = this.innerHTML;
-}
-
-window.addEventListener("onkeydown", (eve) => {
-    eve.preventDefault();
-    var matchesSelectors = document.getElementsByClassName("match");
-    let key = eve.key;
-    for (i = 0; i < matchesSelectors.length; i++) {
-        // if (key === "ArrowDown" && matchesSelectors[i].hasFocus())
-        if (key === "ArrowDown") {
-            // matchesSelectors[i + 1].focus();   
-            console.log("wer");
+function posMatches(val, db)
+{   
+    const mtch = [];
+    for(const x of db.keys()) {
+        if (x.toString().includes(val)) {
+            mtch.push(x.toString());
         }
     }
-})
+    return mtch;
+}
+function selectFromAC() {
+    guessBox.value = this.innerHTML;
+    guessBox.focus();
+}
+function focusedThenHover() {
+    guessBox.focus();
+    hoverAct();
+}
+function hoverDis() {
+    var matchesSelectors = document.getElementsByClassName("match");
+    for (i = 0; i < matchesSelectors.length; i++) {
+        matchesSelectors[i].setAttribute("class", matchesSelectors[i].getAttribute("class") + " no-hover");
+    }
+}
+function hoverAct() {
+    var matchesSelectors = document.getElementsByClassName("match");
+    for (i = 0; i < matchesSelectors.length; i++) {
+        matchesSelectors[i].setAttribute("class", "match");
+    }
+}
+
 sub.addEventListener("click", (e) => {
+    let val = guessBox.value;
     e.preventDefault();
-    labelsCreate(guessCounter);
-    boxesCreate();
-    nameCreate(guessCounter, guessBox.value);
-    guessCounter++;
+    try {
+        if (!(pDB.has(val))) throw "לא נמצא שחקן";
+        labelsCreate(guessCounter);
+        boxesCreate();
+        nameCreate(guessCounter, val);
+        guessCounter++;
+        if (guessWrapper.lastElementChild.getAttribute("id") === "ac-box") {
+            const rmv = guessWrapper.lastElementChild;
+            rmv.remove();
+        }
+    }
+    catch(err) {
+        document.getElementsByClassName("no-match")[0].innerHTML = err;
+    }
 }
 );
 
@@ -98,3 +185,9 @@ function nameCreate(x, val) {
     guessBox.placeholder = "ניחוש מס' " + (x + 1);
 };
 
+/* IDEAS:
+        * autocomplete box to be over the guesses
+        * hovering makes arrow down,up or tab move from the last hovered
+        * close autocomplete box after clicking somewhere else
+        * close ac box after guess box has been emptied with arrow up
+*/
